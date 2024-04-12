44e70c2393893eae92d6f230caf1c2a96e41f296
matheusat98 <166852610+matheusat98@users.noreply.github.com>
2024-04-12 14:12:27 -0300

    Update Map.cs

--- TargetPortal/Map.cs
+++ TargetPortal/Map.cs
@@ -5,15 +5,12 @@ using System.Reflection;
 using Groups;
 using HarmonyLib;
 using UnityEngine;
-
 namespace TargetPortal;
-
 public static class Map
 {
 	public static bool Teleporting;
-	private static readonly Dictionary<Minimap.PinData, ZDO> activePins = new();
+	private static readonly Dictionary<MinimapMinimap.PinDataPinData, ZDO> activePins = new();
 	private static bool[]? visibleIconTypes;
-
 	[HarmonyPatch(typeof(TeleportWorldTrigger), nameof(TeleportWorldTrigger.OnTriggerEnter))]
 	private class OpenMapOnPortalEnter
 	{
@@ -23,73 +20,59 @@ public static class Map
 			{
 				return false;
 			}
-
 			if (TargetPortal.limitToVanillaPortals.Value == TargetPortal.Toggle.On && Utils.GetPrefabName(__instance.transform.parent.gameObject) != "portal_wood")
 			{
 				return true;
 			}
-
 			Teleporting = true;
 			Minimap.instance.ShowPointOnMap(__instance.transform.position);
-
 			AddPortalPins();
-
 			if (InventoryGui.IsVisible())
 			{
 				InventoryGui.instance.Hide();
 			}
-
 			if (TargetPortal.hidePinsDuringPortal.Value == TargetPortal.Toggle.On && visibleIconTypes == null)
 			{
 				visibleIconTypes = new bool[Minimap.instance.m_visibleIconTypes.Length];
 				Array.Copy(Minimap.instance.m_visibleIconTypes, visibleIconTypes, Minimap.instance.m_visibleIconTypes.Length);
 				ToggleIconFilters(true);
 			}
-
 			return false;
 		}
 	}
-
 	private static void ToggleIconFilters(bool force = false)
 	{
 		if (visibleIconTypes == null)
 		{
 			return;
 		}
-
 		HashSet<Sprite> locationSprites = new(Minimap.instance.m_locationIcons.Select(l => l.m_icon));
 		HashSet<int> locationPins = new(Minimap.instance.m_pins.Where(p => locationSprites.Contains(p.m_icon)).Select(p => (int)p.m_type))
 		{
 			AddMinimapPortalIcon.pinType,
 		};
-
 		for (int i = 0; i < visibleIconTypes.Length; ++i)
 		{
 			if (locationPins.Contains(i))
 			{
 				continue;
 			}
-
 			if (visibleIconTypes[i] && (!Minimap.instance.m_visibleIconTypes[i] || force))
 			{
 				Minimap.instance.ToggleIconFilter((Minimap.PinType)i);
 			}
 		}
 	}
-
 	public static void CancelTeleport()
 	{
 		Teleporting = false;
-
 		RemovePortalPins();
-
 		if (TargetPortal.hidePinsDuringPortal.Value == TargetPortal.Toggle.On)
 		{
 			ToggleIconFilters();
 			visibleIconTypes = null;
 		}
 	}
-
 	private static void HandlePortalClick()
 	{
 		if (!Player.m_localPlayer.IsTeleportable())
@@ -97,38 +80,33 @@ public static class Map
 			Player.m_localPlayer.Message(MessageHud.MessageType.Center, "$msg_noteleport");
 			return;
 		}
-
 		foreach (Minimap.PinData pinData in activePins.Keys)
 		{
 			pinData.m_save = true;
 		}
 
 		Minimap Minimap = Minimap.instance;
-		Minimap.PinData? closestPin = Minimap.GetClosestPin(Minimap.ScreenToWorldPoint(Input.mousePosition), Minimap.m_removeRadius * (Minimap.m_largeZoom * 2f));
+		Vector3 ScreenToWorldPoint = Minimap.ScreenToWorldPoint(Input.mousePosition);
+
+        Minimap.PinData? closestPin = Minimap.GetClosestPin(ScreenToWorldPoint, Minimap.m_removeRadius * (Minimap.m_largeZoom * 2f));
 
 		foreach (Minimap.PinData pinData in activePins.Keys)
 		{
 			pinData.m_save = false;
 		}
-
 		if (closestPin is null)
 		{
 			return;
 		}
-
 		if (!activePins.TryGetValue(closestPin, out ZDO portalZDO))
 		{
 			return;
 		}
-
 		Quaternion rotation = portalZDO.GetRotation();
-
 		Minimap.SetMapMode(Minimap.MapMode.Small);
 		CancelTeleport();
-
 		Player.m_localPlayer.TeleportTo(closestPin.m_pos + rotation * Vector3.forward + Vector3.up, rotation, true);
 	}
-
 	[HarmonyPatch(typeof(Minimap), nameof(Minimap.SetMapMode))]
 	public class LeavePortalModeOnMapClose
 	{
@@ -140,19 +118,16 @@ public static class Map
 			}
 		}
 	}
-
 	[HarmonyPatch(typeof(Minimap), nameof(Minimap.Start))]
 	public class AddMinimapPortalIcon
 	{
 		public static int pinType;
-
 		private static void Postfix(Minimap __instance)
 		{
 			pinType = __instance.m_visibleIconTypes.Length;
 			bool[] visibleIcons = new bool[pinType + 1];
 			Array.Copy(__instance.m_visibleIconTypes, visibleIcons, pinType);
 			__instance.m_visibleIconTypes = visibleIcons;
-
 			__instance.m_icons.Add(new Minimap.SpriteData
 			{
 				m_name = (Minimap.PinType)pinType,
@@ -160,7 +135,6 @@ public static class Map
 			});
 		}
 	}
-
 	[HarmonyPatch(typeof(Minimap), nameof(Minimap.OnMapLeftClick))]
 	private class MapLeftClick
 	{
@@ -174,7 +148,6 @@ public static class Map
 			return false;
 		}
 	}
-
 	[HarmonyPatch]
 	private class MapAlternativeClick
 	{
@@ -184,13 +157,11 @@ public static class Map
 			AccessTools.DeclaredMethod(typeof(Minimap), nameof(Minimap.OnMapRightClick)),
 			AccessTools.DeclaredMethod(typeof(Minimap), nameof(Minimap.OnMapMiddleClick)),
 		};
-
 		private static bool Prefix()
 		{
 			return !Teleporting;
 		}
 	}
-
 	[HarmonyPatch(typeof(Minimap), nameof(Minimap.Update))]
 	private static class ShowPortalIcons
 	{
@@ -209,7 +180,6 @@ public static class Map
 			}
 		}
 	}
-
 	private static void AddPortalPins()
 	{
 		string? myId = PrivilegeManager.GetNetworkUserId();
@@ -223,7 +193,6 @@ public static class Map
 			}
 		}
 	}
-
 	private static void RemovePortalPins()
 	{
 		foreach (Minimap.PinData pinData in activePins.Keys)
